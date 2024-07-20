4278248696db85e4ed40f0444a0cf626cf08eb1b
Banudeep-bee <banudeep8422@gmail.com>
2024-07-20 12:19:10 +0530

    commiting the structure

--- public/KSEC/aboutus/aboutus.js
+++
@@ -1,7 +0,0 @@
-// Add active class to the current navigation link
-const navLinks = document.querySelectorAll('nav a');
-navLinks.forEach(link => {
-    if (link.href === window.location.href) {
-        link.classList.add('active');
-    }
-});
